10e656acf9b6fcbb30483842d69c9a06cf7dabf1
mh5986759 <mh5986759@gmail.com>
2017-04-14 19:52:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,8 @@ bots = {
     
     "bot": [4488, "/root/bot/", "root"],
     "esetlua": [4489, "/home/eset/esetlua/", "eset"],
-    #"Totfarangi": [4490, "/home/totfarangi/teleseed/", "totfarangi"],
+    "security": [4488, "/home/securitybot/security/cli/bot/", "securitybot"],
+    "security": [4490, "/home/securitybot/security/api/", "securitybot"],
 	}
 
 	
