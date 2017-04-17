2d37d59f81be2cdb69b3f63cf436817464b68db4
mh5986759 <mh5986759@gmail.com>
2017-04-17 11:12:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ check_time = 14
 
 bots = {
     
-    "bot": [4488, "/root/bot/", "root"],
+    #"bot": [4488, "/root/bot/", "root"],
     "esetlua": [4489, "/home/eset/esetlua/", "eset"],
-    "security": [4488, "/home/securitybot/security/cli/bot/", "securitybot"],
-    "security": [4490, "/home/securitybot/security/api/", "securitybot"],
+    #"security": [4488, "/home/securitybot/security/cli/bot/", "securitybot"],
+    #"security": [4490, "/home/securitybot/security/api/", "securitybot"],
 	}
 
 	
