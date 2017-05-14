0fded0ce5ee769e388d9138f72a4e13b66795e10
mh5986759 <mh5986759@gmail.com>
2017-05-14 16:59:57 +0100

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,6 @@ bots = {
     
     #"bot": [4488, "/root/bot/", "root"],
     "esetlua": [4489, "/root/esetlua/", "root"],
-    "beyond": [4488, "/home/bot2/beyond/", "bot2"],
     #"security": [4490, "/home/securitybot/security/api/", "securitybot"],
 	}
 
