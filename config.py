21a1744ddf797b18827cec902fe072b3457b63dc
mh5986759 <mh5986759@gmail.com>
2017-05-19 10:42:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ check_time = 15
 bots = {
     
     #"bot": [4488, "/root/bot/", "root"],
-    "esetlua": [4489, "/root/esetlua/", "root"],
+    #"esetlua": [4489, "/root/esetlua/", "root"],
     "madator": [4490, "/home/bot2/madator/", "bot2"],
 	}
 
